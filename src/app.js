647c34a2a0ab55677c0ff682e14b635caec95141
yitinglee <ranye0719@gmail.com>
2017-09-12 18:11:16 +0800

    User input is added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginFrom from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBqwPfzy960bBdyZv9u6JOxrmIZuC_rYyk',
+            authDomain: 'authentication-7d01c.firebaseapp.com',
+            databaseURL: 'https://authentication-7d01c.firebaseio.com',
+            projectId: 'authentication-7d01c',
+            storageBucket: 'authentication-7d01c.appspot.com',
+            messagingSenderId: '969518556027'
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>App!</Text>
+                <Header headerText="Authentication" />
+                <LoginFrom />
             </View>
         );
     }
